8ddca14c1d3d9e70e4306fdec93cac82040808fc
m-abdullah-nabeel <muhammadabdullahnabeel@gmail.com>
2023-10-28 17:27:36 +0500

    added first route structure

--- server.js
+++ server.js
@@ -3,24 +3,22 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const mongoUri = 'mongodb+srv://dbUser:dbUserPassword@cluster0.wsriqhq.mongodb.net/?retryWrites=true&w=majority'
+const PORT = 3000
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-    res.json({"message": "Hello Crud Node Express"});
-});
+const stoneRouter = require("./routers/stone")
+app.use('/', stoneRouter);
 
+mongoose.connect(mongoUri, {useNewUrlParser: true})
+const con = mongoose.connection
 
-mongoose.connect(mongoUri)
-.then(()=> {
-    console.log("Coneected to Mongo DB!")
-})
-.catch((error)=>{
-    console.log(error)
+con.on('open', ()=>{
+    console.log("Connected to MongoDB!")
 })
 
-
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
